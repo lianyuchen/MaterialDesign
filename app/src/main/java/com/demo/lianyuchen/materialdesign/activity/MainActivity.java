4ef1d0639159e0f9c72a28a430ca26f8b059785b
lianyuchen <lyc_yuchen@126.com>
2016-08-31 13:57:41 +0800

    完善使用navigationview + fragment的方式，另外加入nav_header样式布局

--- app/src/main/java/com/demo/lianyuchen/materialdesign/activity/MainActivity.java
+++ app/src/main/java/com/demo/lianyuchen/materialdesign/activity/MainActivity.java
@@ -1,51 +1,42 @@
 package com.demo.lianyuchen.materialdesign.activity;
 
-import android.graphics.Color;
+import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.FrameLayout;
-import android.widget.TextView;
 
 import com.demo.lianyuchen.materialdesign.R;
-import com.demo.lianyuchen.materialdesign.adapter.MyAdapter;
 import com.demo.lianyuchen.materialdesign.fragment.FragmentOne;
 import com.demo.lianyuchen.materialdesign.fragment.FragmentThree;
 import com.demo.lianyuchen.materialdesign.fragment.FragmentTwo;
 
 public class MainActivity extends BaseActivity {
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    private String[] myDataset = new String[]{"1", "2", "3", "4", "1", "2", "3", "4", "1", "2", "3", "4", "1", "2", "3", "4"};
+    private CoordinatorLayout rootLayout;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final CoordinatorLayout rootLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        rootLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
 
-        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
-        collapsingToolbarLayout.setTitle("MeterialDesign");
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
         //设置ToolBar
-        final Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
         //设置tabLayout
@@ -60,7 +51,7 @@ public class MainActivity extends BaseActivity {
 //        tabLayout.addTab(tabLayout.newTab().setText("Tab 8"));
 //        tabLayout.addTab(tabLayout.newTab().setText("Tab 9"));
         //设置抽屉DrawerLayout
-        final DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
                 R.string.drawer_open, R.string.drawer_close);
         mDrawerToggle.syncState();//初始化状态
@@ -68,29 +59,9 @@ public class MainActivity extends BaseActivity {
 
         //设置导航栏NavigationView的点击事件
         NavigationView mNavigationView = (NavigationView) findViewById(R.id.navigation);
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.item_one:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentOne()).commit();
-                        collapsingToolbarLayout.setTitle("我的动态");
-                        break;
-                    case R.id.item_two:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentTwo()).commit();
-                        collapsingToolbarLayout.setTitle("我的留言");
-                        break;
-                    case R.id.item_three:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new FragmentThree()).commit();
-                        collapsingToolbarLayout.setTitle("附近的人");
-                        break;
-                }
-                menuItem.setChecked(true);//点击了把它设为选中状态
-                mDrawerLayout.closeDrawers();//关闭抽屉
-                return true;
-            }
-        });
 
+        setupNavigationDrawerContent(mNavigationView);
+        setFragment(0);
         //设置FAB的点击事件
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fabBtn);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -113,6 +84,75 @@ public class MainActivity extends BaseActivity {
 //        // specify an adapter (see also next example)
 //        mAdapter = new MyAdapter(myDataset);
 //        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void setupNavigationDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.item_one:
+                                menuItem.setChecked(true);
+                                setFragment(0);
+                                collapsingToolbarLayout.setTitle("我的动态");
+                                mDrawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_two:
+                                menuItem.setChecked(true);
+                                setFragment(1);
+                                collapsingToolbarLayout.setTitle("我的留言");
+                                mDrawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+                            case R.id.item_three:
+                                menuItem.setChecked(true);
+                                setFragment(2);
+                                collapsingToolbarLayout.setTitle("我的留言");
+                                mDrawerLayout.closeDrawer(GravityCompat.START);
+                                return true;
+
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    private void setFragment(int position) {
+        FragmentManager fragmentManager;
+        FragmentTransaction fragmentTransaction;
+        switch (position) {
+            case 0:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                FragmentOne oneFragment = new FragmentOne();
+                fragmentTransaction.replace(R.id.frame_content, oneFragment);
+                fragmentTransaction.commit();
+
+                break;
+            case 1:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                FragmentTwo twoFragment = new FragmentTwo();
+                fragmentTransaction.replace(R.id.frame_content, twoFragment);
+                fragmentTransaction.commit();
+                break;
+            case 2:
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                FragmentThree threeFragment = new FragmentThree();
+                fragmentTransaction.replace(R.id.frame_content, threeFragment);
+                fragmentTransaction.commit();
+                break;
+            default:
+
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                FragmentOne fragment = new FragmentOne();
+                fragmentTransaction.replace(R.id.frame_content, fragment);
+                fragmentTransaction.commit();
+                break;
+
+        }
     }
 
     @Override
